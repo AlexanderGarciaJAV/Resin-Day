24c853aba7dff67d1ef5ce35aa1e311f8458364b
AlexanderGarciaJAV <118190841+AlexanderGarciaJAV@users.noreply.github.com>
2022-12-17 17:45:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//menu
 document.querySelector('.menu-btn').addEventListener('click', ()=> {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+
+//boton subir
+let botonsubir = document.querySelector('#subir');
+botonsubir.addEventListener('click', () => {
+    document.body.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+        
+    })  
+})
+
+//compras
