a848caf7d0f8eb084c53a1319f667841a584617b
AlexanderGarciaJAV <118190841+AlexanderGarciaJAV@users.noreply.github.com>
2023-01-05 17:30:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+
+
 //menu
 document.querySelector('.menu-btn').addEventListener('click', ()=> {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
+
+
 
 
 //boton subir
@@ -17,5 +21,30 @@ botonsubir.addEventListener('click', () => {
         
     })  
 })
+//preguntas frecuentes
+let question = document.querySelectorAll('.pregunta');
+let btnDropdown = document.querySelectorAll('.pregunta .mas');
+let answer = document.querySelectorAll('.respuesta');
+let parrafo = document.querySelectorAll('.respuesta p');
 
-//compras
+for ( let i = 0; i < btnDropdown.length; i ++ ){
+
+    let altoparrafo = parrafo[i].clientHeight;
+    let switchc = 0;
+
+    btnDropdown[i].addEventListener('click', () => {
+
+        if(switchc == 0){
+            answer[i].style.height = `${altoparrafo}px`;
+            question[i].style.marginBottom = '10px';
+            btnDropdown[i].innerHTML = '<i>-<i>';
+            switchc ++;  
+        }
+        else if(switchc == 1){
+            answer[i].style.height = '0' ;
+            question[i].style.marginBottom = '0';
+            btnDropdown[i].innerHTML = '<i>+<i>';
+            switchc --;  
+        }
+    })
+}
